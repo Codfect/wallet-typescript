9a70ed74ef873e0b5a3200f7034336be32475b36
Oliver <oliverjgodoy@gmail.com>
2021-03-23 11:34:46 -0300

    format layout and color variables

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -3,6 +3,15 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+    
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #FFFFFF;
   }
 
   * {
@@ -11,10 +20,29 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  body {
-    background: var(--background);
-    -webkit-font-smothing: antialiased;
+// font-size: 16px (padrão)
+html {
+  //Quando o usuario estiver com a tela até 1080px -> font-size vai para 93.75%(16 x 0,9375) = 15px
+  @media (max-width: 1080px) {
+    font-size: 73.75%;
   }
 
+  @media (max-width: 720px) {
+    font-size: 87.5%; //14px
+  }
+}
 
+  body {
+    background: var(--background);
+    -webkit-font-smothing: antialiased; 
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disable] { // O que estiver desabilitado na aplicação
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
 `
