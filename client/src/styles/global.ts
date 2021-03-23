0215e497242f25d34b3b8c7351944c6d90fec88e
Oliver <oliverjgodoy@gmail.com>
2021-03-23 11:50:12 -0300

    set fonts

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -37,6 +37,15 @@ html {
     -webkit-font-smothing: antialiased; 
   }
 
+  body, input, textarea, button { //input, textarea, button por padrão não importam a fonte do body
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }
