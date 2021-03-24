eb370ce32a15e3e6639e094ea7128f21e17ce70f
Oliver <oliverjgodoy@gmail.com>
2021-03-24 14:40:03 -0300

    set fake data api with miragejs

--- client/src/components/Transactions/index.tsx
+++ client/src/components/Transactions/index.tsx
@@ -1,28 +1,38 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function Transactions() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
+
   return(
     <Container> 
       <table>
-        <tr>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
 
         <tbody>
-          <td>Dev</td>
-          <td className="up">R$7.000,00</td>
-          <td>Desenvolvimento</td>
-          <td>20/02/2021</td>
-        </tbody>
-
-        <tbody>
-          <td>Dev</td>
-          <td className="down">R$700</td>
-          <td>Desenvolvimento</td>
-          <td>20/02/2021</td>
+          <tr>
+            <td>Dev</td>
+            <td className="up">R$7.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Dev</td>
+            <td className="down">R$700</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
         </tbody>
       </table>
     </Container>
