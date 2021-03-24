d1ac38b38fb2325a236150b817c27be0c4243260
Oliver <oliverjgodoy@gmail.com>
2021-03-24 15:47:47 -0300

    modal new trasaction is working

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,14 +1,31 @@
-import logoSvg from '../../assets/logo.svg'
+import { useState } from 'react';
+import logoSvg from '../../assets/logo.svg';
+import Modal from 'react-modal';
+
 import { Container, Content } from './styles'
 
 export function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleOpenModal() {
+    setIsModalOpen(true);
+  }
+
+  function handleCloseModal() {
+    setIsModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
         <img src={logoSvg} alt="wallet"/>
-        <button type="button"> 
+        <button type="button" onClick={handleOpenModal}> 
           Nova transação
         </button>
+
+        <Modal isOpen={isModalOpen} onRequestClose={handleCloseModal}>
+          <h2>check the modal</h2>
+        </Modal>
       </Content>
     </Container>
   )
