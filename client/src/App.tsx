3a50a2a664dff868bc55d78b5ed33e66f99477c6
Oliver <oliverjgodoy@gmail.com>
2021-03-24 16:12:07 -0300

    type props for modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,31 @@
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard'
+
+import Modal from 'react-modal';
+
 import { GlobalStyle } from './styles/global';
+import { useState } from 'react';
 
 export function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleOpenModal() {
+    setIsModalOpen(true);
+  }
+
+  function handleCloseModal() {
+    setIsModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header functionHandleOpenModal={handleOpenModal}/>
       <Dashboard />
+
+      <Modal isOpen={isModalOpen} onRequestClose={handleCloseModal}>
+        <h2>check the modal</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,31 +1,20 @@
-import { useState } from 'react';
 import logoSvg from '../../assets/logo.svg';
-import Modal from 'react-modal';
 
 import { Container, Content } from './styles'
 
-export function Header() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+interface HeaderProps {
+  functionHandleOpenModal: () => void;
+}
 
-  function handleOpenModal() {
-    setIsModalOpen(true);
-  }
-
-  function handleCloseModal() {
-    setIsModalOpen(false);
-  }
+export function Header({ functionHandleOpenModal }: HeaderProps) {
 
   return (
     <Container>
       <Content>
         <img src={logoSvg} alt="wallet"/>
-        <button type="button" onClick={handleOpenModal}> 
+        <button type="button" onClick={functionHandleOpenModal}> 
           Nova transação
         </button>
-
-        <Modal isOpen={isModalOpen} onRequestClose={handleCloseModal}>
-          <h2>check the modal</h2>
-        </Modal>
       </Content>
     </Container>
   )
